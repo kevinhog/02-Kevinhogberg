4c98122e70636e389fc7c343ca6a21450371a9d8
kevinhog <43174905+kevinhog@users.noreply.github.com>
2021-04-21 15:45:24 +0200

    Add files via upload

---
+++ repository/src/AdvancedCalculatorInterface.java
@@ -0,0 +1,7 @@
+public interface AdvancedCalculatorInterface {
+	public double squareroot(double number);
+	public double raisedToThePowerOf(double base, double exponent);
+	public double absoluteValue(double number);
+	public double square(double number);
+	public double cube(double number);
+}
---
+++ repository/src/BasicCalculatorTest.java
@@ -0,0 +1,227 @@
+import static org.junit.Assert.assertEquals;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class BasicCalculatorTest {
+
+	BasicCalculator calc = new BasicCalculator();	
+	Random rand = new Random();
+	double numberOne = 0.0;
+	double numberTwo = 0.0;
+	double result = 0.0;
+	static DecimalFormat df2 = new DecimalFormat("#.##");
+	
+	//(rand.nextDouble()*-100);
+	
+	@Test
+	public void testBasicAdditionMethodPositive() {
+		System.out.println("\nAddition Positive: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = (rand.nextDouble()* 100);
+			//System.out.println(numberOne);
+			numberTwo = (rand.nextDouble()* 100);
+			//System.out.println(numberTwo);
+			result = numberOne + numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.addition(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n--------------------------------------------------------------------------------------");
+	//}
+	
+	//@Test
+	//public void testBasicAdditionMethodNegative() {
+		System.out.println("\nAddition Negative: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = (rand.nextDouble()* -100);
+			//System.out.println(numberOne);
+			numberTwo = (rand.nextDouble()* -100);
+			//System.out.println(numberTwo);
+			result = numberOne + numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.addition(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n--------------------------------------------------------------------------------------");
+	//}
+	
+	//@Test
+	//public void testBasicAdditionMethodZero() {
+		System.out.println("\nAddition Zero: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = 0;
+			//System.out.println(numberOne);
+			numberTwo = 0;
+			//System.out.println(numberTwo);
+			result = numberOne + numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.addition(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n======================================================================================");
+	}
+	
+	//// testBasicSubtractionMethod ////
+	
+	@Test
+	public void testBasicSubtractionMethodPositive() {
+		System.out.println("\nSubtraction Positive: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = (rand.nextDouble()* 100);
+			//System.out.println(numberOne);
+			numberTwo = (rand.nextDouble()* 100);
+			//System.out.println(numberTwo);
+			result = numberOne - numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.subtraction(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n--------------------------------------------------------------------------------------");
+	//}
+	//@Test
+	//public void testBasicSubtractionMethodNegative() {
+		System.out.println("\nSubtraction Negative: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = (rand.nextDouble()* -100);
+			//System.out.println(numberOne);
+			numberTwo = (rand.nextDouble()* -100);
+			//System.out.println(numberTwo);
+			result = numberOne - numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.subtraction(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n--------------------------------------------------------------------------------------");
+	//}
+	//@Test
+	//public void testBasicSubtractionMethodZero() {
+		System.out.println("\nSubtraction Zero: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = 0;
+			//System.out.println(numberOne);
+			numberTwo = 0;
+			//System.out.println(numberTwo);
+			result = numberOne - numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.subtraction(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n======================================================================================");
+	}
+	
+	//// testBasicMultiplicationMethod ////
+	@Test
+	public void testBasicMultiplicationMethodPositive() {
+		System.out.println("\nMultiplication Positive: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = (rand.nextDouble()* 100);
+			//System.out.println(numberOne);
+			numberTwo = (rand.nextDouble()* 100);
+			//System.out.println(numberTwo);
+			result = numberOne * numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.multiplication(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n--------------------------------------------------------------------------------------");
+	//}
+	
+	//@Test
+	//public void testBasicMultiplicationMethodNegative() {
+		System.out.println("\nMultiplication Negative: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = (rand.nextDouble()* -100);
+			//System.out.println(numberOne);
+			numberTwo = (rand.nextDouble()* -100);
+			//System.out.println(numberTwo);
+			result = numberOne * numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.multiplication(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n--------------------------------------------------------------------------------------");
+	//}
+	
+	//@Test
+	//public void testBasicMultiplicationMethodZero() {
+		System.out.println("\nMultiplication Zero: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = 0;
+			//System.out.println(numberOne);
+			numberTwo = 0;
+			//System.out.println(numberTwo);
+			result = numberOne * numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.multiplication(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n======================================================================================");
+	}
+	
+	//// testBasicDivisionMethod ////
+	
+	@Test
+	public void testBasicDivisionMethodPositive() {
+		
+		System.out.println("\nDivision Positive: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = (rand.nextDouble()* 100);
+			//System.out.println(numberOne);
+			numberTwo = (rand.nextDouble()* 100);
+			//System.out.println(numberTwo);
+			result = numberOne / numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.division(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n--------------------------------------------------------------------------------------");
+	//}
+	
+	//@Test
+	//public void testBasicDivisionMethodNegativeNegative() {
+		System.out.println("\nDivision Negative: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = (rand.nextDouble()* -100);
+			//System.out.println(numberOne);
+			numberTwo = (rand.nextDouble()* -100);
+			//System.out.println(numberTwo);
+			result = numberOne / numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.division(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\n--------------------------------------------------------------------------------------");
+	//}
+	
+	//@Test
+	//public void testBasicDivisionMethodZero() {
+		System.out.println("\nDivision Zero: ");
+		
+		for (int i = 0; i < 10; i++) {
+			numberOne = 0;
+			//System.out.println(numberOne);
+			numberTwo = 0;
+			//System.out.println(numberTwo);
+			result = numberOne / numberTwo;
+			System.out.print(df2.format(result) + " | " );
+			
+			assertEquals(calc.division(numberOne, numberTwo), result, 0);
+		}
+		System.out.print("\nZero divided by zero is undefined because everything is a valid solution!");
+		System.out.print("\n======================================================================================");
+	}
+}
